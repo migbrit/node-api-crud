3950339c4bad6b760b065fd481ba8b6aae5c874a
Miguel Brito <miguelfelipe_steam@outlook.com>
2023-08-16 16:26:11 -0300

    feat: listing products endpoint

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
 
+router.get('/', productController.get);
+
 router.post('/', productController.post);
   
 router.put('/:id', productController.put);
