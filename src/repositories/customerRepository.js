234a63b5388597f37f33554899f9f6a54fb6916d
Miguel Brito <miguelfelipe_steam@outlook.com>
2023-08-27 22:21:46 -0300

    fix: removing brackets

--- src/repositories/customerRepository.js
+++ src/repositories/customerRepository.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Customer = mongoose.model('Customer');
 
-exports.create = async(body) => {
-    var customer = new Customer({body});
+exports.create = async(data) => {
+    var customer = new Customer(data);
     await customer.save();
 }
